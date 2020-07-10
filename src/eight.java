765f6065230c9d3319fbee640bdcaada472274e4
Ali Kamel <alik87932@gmail.com>
2020-07-10 02:54:04 +0100

    lesson eight (embedding layouts)

---
+++ src/eight.java
@@ -0,0 +1,47 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class eight extends Application{
+    Stage window;
+    public static void main(String[] args){
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage){
+        window = primaryStage;
+        window.setTitle("kamVelo");
+
+        HBox topMenu = new HBox();
+
+        Button buttonA = new Button("File");
+        Button buttonB = new Button("Edit");
+        Button buttonC = new Button("View");
+        topMenu.getChildren().addAll(buttonA,buttonB,buttonC);
+
+        VBox sideMenu = new VBox();
+
+        Button buttonD = new Button("D");
+        Button buttonE = new Button("E");
+        Button buttonF = new Button("F");
+        sideMenu.getChildren().addAll(buttonD,buttonE,buttonF);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTop(topMenu);
+        borderPane.setLeft(sideMenu);
+
+
+        Scene scene = new Scene(borderPane, 300, 250);
+        window.setScene(scene);
+        window.show();
+
+    }
+}
